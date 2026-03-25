23566a4c03138eed6068f5760de3776b6eef31c8
Mann Parekh <msparekh25@gmail.com>
2026-03-25 10:34:27 -0400

    Revert "Rebuild portfolio with liquid glass SaaS redesign"
    This reverts commit 864bf421023eb8d77d74dae4308ff2c09e5ecba1.

--- portfolio-source/src/main.tsx
+++ portfolio-source/src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import '@fontsource/geist-sans/400.css'
-import '@fontsource/geist-sans/500.css'
-import '@fontsource/geist-sans/600.css'
-import '@fontsource/geist-sans/700.css'
 import './index.css'
 import App from './App'
 
-const queryClient = new QueryClient()
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
 )
